c497055256c768e486cdc19d9ff09d34e41bd937
YamatoYamasugi <yamato.yamasugi@optim.co.jp>
2025-04-17 14:55:49 +0900

    feat: Memoモデルに対するテストを追加

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Memo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a title" do
+    memo = Memo.new(title: "Sample title")
+    expect(memo).to be_valid
+  end
+
+  it 'is invalid without a title' do
+    memo = Memo.new(title: nil)
+    memo.valid?
+    expect(memo.errors[:title]).to include("can't be blank")
+  end
 end
