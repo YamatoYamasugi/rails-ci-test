bc91ff22981b59ea79e67b4578d18e21cbce5be1
YamatoYamasugi <yamato.yamasugi@optim.co.jp>
2025-04-18 14:11:23 +0900

    feat: メモ編集機能とテストを実装

--- app/controllers/api/v1/memos_controller.rb
+++ app/controllers/api/v1/memos_controller.rb
@@ -20,6 +20,18 @@ class Api::V1::MemosController < ApplicationController
             render json: { error: "Failed to delete resource" }, status: :unprocessable_entity
         end
     end
+    def update
+        # クライアントより渡されたパラメーター`id`の値を用いて検索したレコードを取得
+        @memo = Memo.find(params[:id])
+        # クライアントからのリクエストパラメータを取得しその値を用いてレコードをアップデート
+        if @memo.update(memo_params)
+            # 更新が成功した場合、更新したレコードをJSONで返す
+            render json: @memo, status: :ok
+        else
+            # 更新が失敗した場合はエラーを返す
+            render json: @memo.errors, status: :bad_request
+        end
+    end
     private
         # ストロングパラメータを使うことで、限られた入力だけに絞ることができる
         def memo_params
